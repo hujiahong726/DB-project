2cbe237b2a42f15471b7b0ba77a73dc99371a20b
hujiahong726 <hu0201da@gmail.com>
2024-04-21 01:40:53 -0400

    create table
    deleted the csv files because we need to use insert to put data in the tables

---
+++ create_tables.sql
@@ -0,0 +1,95 @@
+CREATE TABLE Users (
+    UserID SERIAL PRIMARY KEY,
+    UserName VARCHAR(100),
+    Password VARCHAR(100),
+    Address VARCHAR(255),
+    Latitude DECIMAL(9,6),
+    Longitude DECIMAL(9,6),
+    Profile TEXT,
+    Photo BYTEA
+);
+
+CREATE TABLE UserActivity (
+    UserID INT REFERENCES Users(UserID),
+    LastAccessTimestamp TIMESTAMP,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+CREATE TABLE Neighborhoods (
+    NeighborhoodID SERIAL PRIMARY KEY,
+    Name VARCHAR(100)
+);
+
+CREATE TABLE Blocks (
+    BlockID SERIAL PRIMARY KEY,
+    Name VARCHAR(100),
+    CenterLatitude DECIMAL(9,6),
+    CenterLongitude DECIMAL(9,6),
+    Radius DECIMAL(5,2),
+    NeighborhoodID INT REFERENCES Neighborhoods(NeighborhoodID)
+);
+
+CREATE TABLE UserBlocks (
+    UserID INT REFERENCES Users(UserID),
+    BlockID INT REFERENCES Blocks(BlockID),
+    IsJoined BOOLEAN,
+    PRIMARY KEY (UserID, BlockID)
+);
+
+CREATE TABLE BlockRequests (
+    RequestID SERIAL PRIMARY KEY,
+    SenderID INT REFERENCES Users(UserID),
+    BlockID INT REFERENCES Blocks(BlockID),
+    ApprovedCount INT
+);
+
+CREATE TABLE UserFriends (
+    UserID1 INT,
+    UserID2 INT,
+    FOREIGN KEY (UserID1) REFERENCES Users(UserID),
+    FOREIGN KEY (UserID2) REFERENCES Users(UserID),
+    PRIMARY KEY (UserID1, UserID2)
+);
+
+CREATE TYPE status AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
+
+CREATE TABLE FriendRequests (
+    RequestID SERIAL PRIMARY KEY,
+    SenderID INT REFERENCES Users(UserID),
+    ReceiverID INT REFERENCES Users(UserID),
+    RequestStatus status
+);
+
+CREATE TABLE UserNeighbors (
+    UserID1 INT,
+    UserID2 INT,
+    FOREIGN KEY (UserID1) REFERENCES Users(UserID),
+    FOREIGN KEY (UserID2) REFERENCES Users(UserID),
+    PRIMARY KEY (UserID1, UserID2)
+);
+
+CREATE TABLE Threads (
+    ThreadID SERIAL PRIMARY KEY,
+    LocationLatitude DECIMAL(9,6),
+    LocationLongitude DECIMAL(9,6)
+);
+
+CREATE TABLE Messages (
+    MessageID SERIAL PRIMARY KEY,
+    ThreadID INT REFERENCES Threads(ThreadID),
+    Title VARCHAR(255),
+    AuthorID INT REFERENCES Users(UserID),
+    Timestamp TIMESTAMP,
+    Body TEXT
+);
+
+CREATE TYPE target AS ENUM ('friend', 'neighbor', 'hood', 'block');
+
+CREATE TABLE ThreadRecipients (
+    ThreadID INT REFERENCES Threads(ThreadID),
+    RecipientID INT REFERENCES Users(UserID),
+    LastViewTimestamp TIMESTAMP,
+    Target target,
+    PRIMARY KEY (ThreadID, RecipientID)
+);
+
