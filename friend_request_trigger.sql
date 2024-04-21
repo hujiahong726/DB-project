4b3aec7a57b46d978d9338f7696f92c2eeb0ad9b
hujiahong726 <hu0201da@gmail.com>
2024-04-21 17:50:20 -0400

    friend request trigger
    Created friend request trigger that checks if already friend or request already exists.

---
+++ friend_request_trigger.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION check_friend() RETURNS TRIGGER AS $$
+BEGIN
+	IF EXISTS(SELECT 1 FROM FriendRequests WHERE SenderID = new.SenderID AND ReceiverID = new.ReceiverID) then
+		RAISE EXCEPTION 'Request Failed: Request already sent';
+		return NULL;
+	ELSIF EXISTS(SELECT 1 FROM UserFriends WHERE (UserID1 = new.SenderID AND UserID2 = new.ReceiverID) OR (UserID1 = new.ReceiverID AND UserID2 = new.SenderID)) then
+		RAISE EXCEPTION 'Request Failed: User is friend';
+		return NULL;
+	ELSE
+		return new;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_friend_trigger BEFORE INSERT ON FriendRequests
+FOR EACH ROW
+EXECUTE FUNCTION check_friend();
